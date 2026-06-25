3495f0525ec9c7f03fa63876e81b65f0f93ff3e6
Ramziddinergashaliyev <155714547+Ramziddinergashaliyev@users.noreply.github.com>
2026-06-25 16:18:22 +0500

    add hero imgs

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,12 +1,13 @@
-import "./Hero.scss";
+import "./hero.scss";
 
 function Hero() {
     return (
         <section className="hero">
             <div className="container hero__container">
-                <h1 className="hero__title">
-                    TRT Parts
-                </h1>
+                <h1 className="hero__container-title">THE POWER OF PERFORMANCE.
+                    ENGINEERED TO WIN.</h1>
+                <p className="hero__container-text">Explore our full range of advanced automotive fluids.</p>
+                <button className="hero__container-btn">VIEW PRODUCT PORTFOLIO</button>
             </div>
         </section>
     );
