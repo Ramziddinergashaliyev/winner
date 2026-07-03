e04a8b8f8cc13660f91d4c621c6eee0f3e796c0a
Ramziddinergashaliyev <155714547+Ramziddinergashaliyev@users.noreply.github.com>
2026-07-03 08:47:45 +0500

    add change leazy loading

--- src/components/leazy/Leazy.jsx
+++ src/components/leazy/Leazy.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./leazy.scss";
+import img from "../../assets/video/leazy2.gif";
 
 const Leazy = () => {
     return (
-        <div>Leazy</div>
-    )
-}
+        <div className="leazy">
+            <div className="leazy__card">
+                <img src={img} alt="leazy__card-img" />
+            </div>
+        </div>
+    );
+};
 
-export default Leazy
+export default Leazy;
