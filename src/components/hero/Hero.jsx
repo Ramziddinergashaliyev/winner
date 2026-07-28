31e4e37c40dca3f6d11ca8bd0abb016afd379e63
Ramziddinergashaliyev <155714547+Ramziddinergashaliyev@users.noreply.github.com>
2026-07-28 15:40:30 +0500

    Add

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,3 +1,69 @@
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
+
+// import heroImg1 from "../../assets/images/hero/bannerOne.webp";
+// import heroImg2 from "../../assets/images/hero/bannerTwo.webp";
+// import heroImg3 from "../../assets/images/hero/bannerThree.webp";
+
+// import "./hero.scss";
+
+// const slides = [
+//     {
+//         image: heroImg1,
+//         title: "MOTOR OIL EVERY DRIVE EVERY VICTORY",
+//         text: "Explore our full range of advanced automotive fluids.",
+//     },
+//     {
+//         image: heroImg2,
+//         title: "DIESEL ENGINE OIL POWER FOR EVERY LOAD",
+//         text: "Premium lubricants trusted by professionals worldwide.",
+//     },
+//     {
+//         image: heroImg3,
+//         title: "TRANSMISSION FLUID SHIFT WITH CONFIDENCE",
+//         text: "Advanced formulas designed to protect your engine.",
+//     }
+// ];
+
+// function Hero() {
+//     return (
+//         <section className="hero">
+//             <Swiper
+//                 modules={[Autoplay, Pagination, Navigation]}
+//                 speed={1000}
+//                 loop={true}
+//                 autoplay={{
+//                     delay: 4000,
+//                     disableOnInteraction: false,
+//                 }}
+//                 pagination={{ clickable: true }}
+//                 className="hero__swiper"
+//             >
+//                 {slides.map((slide, index) => (
+//                     <SwiperSlide key={index}>
+
+//                         <div
+//                             className="hero__slide"
+//                             style={{ backgroundImage: `url(${slide.image})` }}
+//                         >
+//                             <div className="hero__slide-info container">
+//                                 <h3 className="hero__slide-info-text">{slide.title}</h3>
+//                             </div>
+//                         </div>
+//                     </SwiperSlide>
+//                 ))}
+//             </Swiper>
+//         </section>
+//     );
+// }
+
+// export default Hero;
+
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
@@ -14,17 +80,17 @@ import "./hero.scss";
 const slides = [
     {
         image: heroImg1,
-        title: "THE POWER OF PERFORMANCE. ENGINEERED TO WIN.",
+        title: "MOTOR OIL EVERY DRIVE EVERY VICTORY",
         text: "Explore our full range of advanced automotive fluids.",
     },
     {
         image: heroImg2,
-        title: "BUILT FOR EXTREME CONDITIONS.",
+        title: "TRANSMISSION FLUID SHIFT WITH CONFIDENCE",
         text: "Premium lubricants trusted by professionals worldwide.",
     },
     {
         image: heroImg3,
-        title: "INNOVATION IN EVERY DROP.",
+        title: "DIESEL ENGINE OIL POWER FOR EVERY LOAD",
         text: "Advanced formulas designed to protect your engine.",
     }
 ];
@@ -45,10 +111,14 @@ function Hero() {
             >
                 {slides.map((slide, index) => (
                     <SwiperSlide key={index}>
+
                         <div
                             className="hero__slide"
                             style={{ backgroundImage: `url(${slide.image})` }}
                         >
+                            <div className="hero__slide-info container">
+                                <h3 className="hero__slide-info-text">{slide.title}</h3>
+                            </div>
                         </div>
                     </SwiperSlide>
                 ))}
