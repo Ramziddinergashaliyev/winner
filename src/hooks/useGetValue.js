494dc915cdd70f3139daff46c78ceede9f61f18f
Ramziddinergashaliyev <155714547+Ramziddinergashaliyev@users.noreply.github.com>
2026-08-27 10:58:11 +0500

    add change full banner

--- src/hooks/useGetValue.js
+++ src/hooks/useGetValue.js
@@ -1,13 +1,13 @@
 import { useState } from "react"
 
 export const useGetValue = (initialState) => {
-    const { formData, setFormData } = useState(initialState)
+    const [formData, setFromData] = useState(initialState)
 
     const handleChange = (e) => {
         const { name, value } = e.target;
 
-        setFormData((prev) => ({ ...prev, [name]: value }))
+        setFromData((prev) => ({ ...prev, [name]: value }))
     }
 
-    return { formData, setFormData, handleChange }
+    return { formData, setFromData, handleChange }
 }
